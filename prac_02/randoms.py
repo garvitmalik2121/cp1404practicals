1f17239588b4e4ea8bad3266f2eaf9d712e28375
garvit_malik <garvit.malik@my.jcu.edu.au>
2023-06-13 21:50:52 +1000

    write the answers of questions in comment

---
+++ prac_02/randoms.py
@@ -0,0 +1,24 @@
+"""
+CP1404/CP5632 - Practical
+
+In your console, type in the following (run each line multiple times), and write the answers to the questions below in comments in randoms.py.
+
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3"""
+
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+# smallest number is 5 and highest number is 20
+#
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+# smallest number is three nd the largest is 9 and it does not produce 4
+#
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+# I have seen Smallest number is 2.653325565344101 and the largest number is 5.387437042155433
+#
