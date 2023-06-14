4d7fca2ed450c89be924656b90b421e4b24cbeee
garvit_malik <garvit.malik@my.jcu.edu.au>
2023-06-14 19:37:36 +1000

    add loop to avoid ZeroDivisionError

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -2,17 +2,20 @@
 CP1404/CP5632 - Practical
 Answer the following questions:
 1. When will a ValueError occur?
+Answer: it will occur when we enter a input type other than int for numerator or the denominator
 2. When will a ZeroDivisionError occur?
+Answer: it will occur when user enter 0 as input for denominator
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+Answer: 0
 """
 
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
-    fraction = numerator / denominator
-    print(fraction)
+    if denominator == 0:
+        print("invalid Denominator ")
+    else:
+        fraction = numerator / denominator
+        print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
-print("Finished.")
