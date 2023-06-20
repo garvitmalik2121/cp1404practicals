26830d8466dff931b0e0f936373e65bf3d2612a5
garvit_malik <garvit.malik@my.jcu.edu.au>
2023-06-20 13:57:50 +1000

    change the loop

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -6,16 +6,16 @@ Answer: it will occur when we enter a input type other than int for numerator or
 2. When will a ZeroDivisionError occur?
 Answer: it will occur when user enter 0 as input for denominator
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
-Answer: 0
+Answer: At 0
 """
 
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
-    if denominator == 0:
+    while denominator == 0:
         print("invalid Denominator ")
-    else:
-        fraction = numerator / denominator
-        print(fraction)
+        denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+    print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
