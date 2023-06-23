8aea06d1390e3004462ad18a80a03ed4eed10d1e
garvit_malik <garvit.malik@my.jcu.edu.au>
2023-06-24 07:50:12 +1000

    update style of printing

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,29 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    print_subject_details(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    data = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        data.append(parts)  # add parts into list
     input_file.close()
+    return data
+
+
+def print_subject_details(data):
+    """Print subject details: subject code, lecturer name, and number of students."""
+    for subject in data:
+        subject_code = subject[0]
+        lecturer_name = subject[1]
+        number_of_students = subject[2]
+        print(f"{subject_code} is taught by {lecturer_name} and has {number_of_students} students")
 
 
 main()
