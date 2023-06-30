8053688953684431292542ea75b13f8be95c576c
garvit_malik <garvit.malik@my.jcu.edu.au>
2023-06-30 10:21:50 +1000

    complete prac5 exercise word occurrences

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,21 @@
+"""
+CP1404/CP5632 Practical
+Word Count
+
+Word Occurrences
+Estimate: 40 minutes
+Actual:  1 hour 30 minutes
+
+"""
+text = input("text: ")
+words = text.split()  # Split the string into words
+word_to_count = {}
+for word in words:
+    if word in word_to_count:
+        word_to_count[word] += 1
+    else:
+        word_to_count[word] = 1
+# Determine the width for aligning the output
+length_longest_word = max(len(word) for word in word_to_count)
+for word, count in sorted(word_to_count.items()):
+    print(f"{word:{length_longest_word}} : {count}")
